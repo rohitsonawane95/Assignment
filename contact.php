305041d2354581b1a0391c0bc269d3695e1f9d4c
rohitsonawane95 <96277225+rohitsonawane95@users.noreply.github.com>
2021-12-17 11:49:38 +0530

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<title>Contact Form</title>
+</head>
+<body>
+<form action="data.php" method="POST">
+<div class="container">
+<h1> Contact Form</h1>	
+<label>Title</label>
+<!--for drop-down list select tag is use-->
+<select name="title1">	
+<option value="Mr">Mr</option>
+<option value="Mrs">Mrs</option>
+<option value="Ms">Ms</option>
+</select><br>
+<!-- input tag use for create input field--->
+<label>Firstname :*</label>
+<!--- user required to fill * mark field---->
+<input type="text" name="fn" placeholder="enter your first name" required><br>
+<label>Lastname :*</label>
+<input type="text" name="ln" placeholder="enter your last name" required><br>
+<label>Date of Birth :</label>
+<input type="date" name="DOB"><br>
+<label>Telephone Number : *</label>
+<input type="tel" name="Number" placeholder="Enter Your Number" maxlength="10" required><br>
+<label>File upload (only pdf and images) :</label>
+<!---only pdf and images accept in this field---> 
+<input type="file" onchange="readURL(this);" name="File" accept=".jpg,.pdf" required><br>
+<label>Email : </label>
+<input type="text" name="em" placeholder="enter your email id" required ><br>
+<!-- Maxlength attribute used to accept maximum 300 charaters--->
+<label>Message :</label> 
+<input type="text" name="msg" maxlength="300"><br>
+<input type="submit" name="submit">
+</div>	
+</form>	
+</body>
+</html>
---
+++ data.php
@@ -0,0 +1,30 @@
+
+//for display selected value in next page
+<?php
+
+    if(isset($_POST['submit']))
+    {
+        $gettitle=$_POST['title1'];
+    }    
+?>
+
+<?php
+//creating variable and assign method
+$title = $_POST['title1'];
+$Firstname = $_POST['fn'];
+$Lastname = $_POST['ln'];
+$Date = $_POST['DOB'];
+$Email = $_POST['em'];
+$Telephone= $_POST['Number'];
+$Files = $_POST['File'];
+$Message = $_POST['msg'];
+//for display data in next webpage 
+
+echo "Thank You ".$title. $Firstname.$Lastname."<br>";
+echo "You Have submitted the following details:"."<br>";
+echo "Date of Birth :  ".$Date."<br>";
+echo "Telephone  :  ".$Telephone."<br>";
+echo " File Uploaded :  ".$Files."<br>";
+echo "Message :  ".$Message."<br>";
+
+?>
